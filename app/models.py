7cc5bbd6c544aa7496626d14358b8d30ffdde470
Aman Javed Maner <97833750+amanmaner011@users.noreply.github.com>
2024-07-11 19:26:17 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from app import db
 
 class CVE(db.Model):
+
+    __tablename__ = 'cves'
+    
     cve_id = db.Column(db.String, primary_key=True)
     severity = db.Column(db.String)
     cvss = db.Column(db.Float)
